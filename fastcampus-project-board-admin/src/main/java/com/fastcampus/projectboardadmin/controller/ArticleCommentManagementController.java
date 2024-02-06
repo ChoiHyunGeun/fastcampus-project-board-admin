b28b0940c03cf20e9a10d7f2714b90fdfa39d1f0
hgChoi <chlgusrms96@naver.com>
2024-02-06 10:42:25 +0900

    #13 - 뷰 컨트롤러, 기본 뷰 작성
    어드민에서 필요로 한 뷰는 다음 5가지
    
    * 메인 루트 뷰
    * 게시글 관리 뷰
    * 댓글 관리 뷰
    * 회원 관리 뷰
    * 어드민 회원 뷰
    
    이에 각 뷰를 담당할 컨트롤러 클래스와
    기본적인 최소한의 핸들러 메소드, 뷰 템플릿을 작성함.
    실제 비즈니스 로직 구현을 담고 있지 않음

---
+++ fastcampus-project-board-admin/src/main/java/com/fastcampus/projectboardadmin/controller/ArticleCommentManagementController.java
@@ -0,0 +1,11 @@
+package com.fastcampus.projectboardadmin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("/management/article-comments")
+@Controller
+public class ArticleCommentManagementController {
+}
