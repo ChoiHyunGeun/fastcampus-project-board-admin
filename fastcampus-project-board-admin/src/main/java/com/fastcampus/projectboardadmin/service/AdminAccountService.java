55ee6e7eec166889fc00827e7a3783d62c6d46a2
hgChoi <chlgusrms96@naver.com>
2024-02-19 15:37:46 +0900

    #37 - 어드민 회원 기능, 컨트롤러, 뷰 구현

--- fastcampus-project-board-admin/src/main/java/com/fastcampus/projectboardadmin/service/AdminAccountService.java
+++ fastcampus-project-board-admin/src/main/java/com/fastcampus/projectboardadmin/service/AdminAccountService.java
@@ -6,6 +6,7 @@ import com.fastcampus.projectboardadmin.dto.AdminAccountDto;
 import com.fastcampus.projectboardadmin.repository.AdminAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ public class AdminAccountService {
 
     private final AdminAccountRepository adminAccountRepository;
 
+    @Transactional(readOnly = true)
     public Optional<AdminAccountDto> searchUser(String username) {
         return adminAccountRepository.findById(username).map(AdminAccountDto::from);
     }
@@ -27,6 +29,7 @@ public class AdminAccountService {
         );
     }
 
+    @Transactional(readOnly = true)
     public List<AdminAccountDto> users() {
         return adminAccountRepository.findAll().stream()
                 .map(AdminAccountDto::from)
